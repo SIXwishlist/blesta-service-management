34a0ced7c24c90d96a0e4171e4a3b8165f2cf4d6
bo1 <wanbo.mail@gmail.com>
2018-06-12 15:22:59 +1000

    multiple tabs

--- live_answering_plugin.php
+++ live_answering_plugin.php
@@ -1,47 +1,56 @@
 <?php
 /**
- * Support Manager plugin handler
- *
- * @package blesta
- * @subpackage blesta.plugins.live_answering
- * @copyright Copyright (c) 2010, Phillips Data, Inc.
- * @license http://www.blesta.com/license/ The Blesta License Agreement
- * @link http://www.blesta.com/ Blesta
- */
+* Support Manager plugin handler
+*
+* @package blesta
+* @subpackage blesta.plugins.live_answering
+* @copyright Copyright (c) 2010, Phillips Data, Inc.
+* @license http://www.blesta.com/license/ The Blesta License Agreement
+* @link http://www.blesta.com/ Blesta
+*/
 class LiveAnsweringPlugin extends Plugin
 {
-    public function __construct()
-    {
+  public function __construct()
+  {
 
-        Language::loadLang('live_answering_plugin', null, dirname(__FILE__) . DS . 'language' . DS);
-
-
-        $this->loadConfig(dirname(__FILE__) . DS . 'config.json');
-    }
+    Language::loadLang('live_answering_plugin', null, dirname(__FILE__) . DS . 'language' . DS);
 
 
-    public function install($plugin_id)
-    {
-    	 Configure::load('live_answering', dirname(__FILE__) . DS . 'config' . DS);
-    }
+    $this->loadConfig(dirname(__FILE__) . DS . 'config.json');
+  }
 
-    public function getActions()
-     {
-        	return [
-	        	[
-	                'action' => 'nav_primary_client',
-	                'uri' => 'plugin/live_answering/client_main/',
-	                'name' => 'Live Answering'
-	            ]
-            ],
 
-            [
-	                'action' => 'nav_new_tab',
-	                'uri' => 'plugin/live_answering/new_tab/',
-	                'name' => 'New Tab'
-	            ]
-            ];
-      }
+  public function install($plugin_id)
+  {
+    Configure::load('live_answering', dirname(__FILE__) . DS . 'config' . DS);
+  }
+
+  public function getActions()
+  {
+    /*
+    return [
+      [
+        'action' => 'nav_primary_client',
+        'uri' => 'plugin/live_answering/client_main/',
+        'name' => 'Live Answering'
+      ]
+    ];
+    */
+
+    return [
+      [
+        'action' => 'nav_primary_client',
+        'uri' => 'plugin/live_answering/client_main/',
+        'name' => 'Live Answering'
+      ]
+    ,
+    [
+      'action' => 'nav_new_tab',
+      'uri' => 'plugin/live_answering/new_tab/',
+      'name' => 'New Tab'
+    ]
+  ];
+}
 
 
 }
